ab483716d47151e3f87e6a4867b5d634ab6e60be
Carsten <carsten@kopis.de>
2015-04-25 16:22:54 +0200

    Update stripmine.lua with better turns.
    Still not competed, the robot is not stopping after number of strips...

--- stripmine.lua
+++ stripmine.lua
@@ -12,24 +12,31 @@ local gap = 2
 local torch = 7
 
 -- do not touch!
+local currentStrip = 0
 
 local function turn()
   -- turn every 2nd into the other direction
-  if (strips % 2 == 0) then
-    robot.turnLeft()
-  else
+  if (currentStrip % 2 == 0) then
     robot.turnRight()
+  else
+    robot.turnLeft()
+  end
+end
+
+local function turnAround()
+  for i = 0, 1 do
+    robot.turnLeft()
   end
 end
 
 local function mineHeight(height)
   for y = 0, (height - 1) do
     -- first swing your tool
-    while(robot.detect()) do
+    if(robot.detect()) then
       robot.swing()
     end
     -- then go up
-    while(robot.detectUp()) do
+    if(robot.detectUp()) then
       robot.swingUp()
     end
     robot.up()
@@ -42,25 +49,31 @@ end
 
 -- start to stripmine
 for z = 0, strips do
--- start a new strip
-for x = 0, length do
+  -- start a new strip
+  for x = 0, length do
+    mineHeight(3)
+
+    -- TODO select torch from inventor
+    -- TODO place torch to the left
+
+    -- forward
+    robot.forward()
+  end
+  -- end this strip
+  turn()
+
+  -- connect previous strip
+  turnAround()
   mineHeight(3)
+  turnAround()
 
-  -- TODO select torch from inventor
-  -- TODO place torch to the left
+  for x = 0, gap do
+    mineHeight(3)
+    robot.forward()
+  end
+  turn()
 
-  -- forward
-  robot.forward()
+  -- start next strip
+  currentStrip = currentStrip + 1
 end
--- end this strip
-turn()
-
-for x = 0, gap do
-  mineHeight(3)
-  robot.forward()
-end
-turn()
-
-end
-
 
